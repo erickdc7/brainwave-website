9f7cf7f324e4cb6a955e26ae20294885466cd631
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-07-29 17:53:59 -0500

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-function App() {
+const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+};
 
 export default App
