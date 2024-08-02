f4b6d268e45fe09f5bd1ab900e18b6a64e233224
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-08-02 12:55:53 -0500

    add button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
-const Button = () => {
-    return (
-        <div>Button</div>
+import ButtonSvg from '../assets/svg/ButtonSvg'
+
+const Button = ({ className, href, onClick, children, px, white }) => {
+    const renderButton = () => (
+        <button>
+            <span>{children}</span>
+            {ButtonSvg(white)}
+        </button>
     )
+
+    return renderButton
 }
 
 export default Button
