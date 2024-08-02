859661d6541f50d8587883a5874aba44bc82f79f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-08-02 13:33:57 -0500

    add styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+
+        </div>
     )
 }
 
