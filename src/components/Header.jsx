da5a606dc4f10335a6c698b7264d6564dfc21bac
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-08-02 13:42:06 -0500

    add nav styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,10 @@ const Header = () => {
                 <a className='block w-[12rem] xl:mr-8' href='#hero'>
                     <img src={brainwave} width={190} height={40} alt='Brainwave' />
                 </a>
+
+                <nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
+
+                </nav>
             </div>
         </div>
     )
